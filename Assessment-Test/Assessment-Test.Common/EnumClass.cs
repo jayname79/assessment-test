87dc0b06a083c01b074a06a68bd00df04d9b8008
jayname79 <61588151+jayname79@users.noreply.github.com>
2020-09-15 22:52:44 +0530

    Common - Added Enums for Promotion Rule

---
+++ Assessment-Test/Assessment-Test.Common/EnumClass.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Assessment_Test.Common
+{
+    /// <summary>
+    /// Enum for Promotion rule
+    /// </summary>
+    public enum EnumPromotionRuleType
+    {
+        Comb = 1,
+        OnePlusOne = 2
+    }
+}
